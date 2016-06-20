07334f2ea22050c4c41b3e85be525ca7058daa9c
Dave Gamberg <dgamberg@gmail.com>
2016-06-20 14:15:50 -0500

    added child components and refactored

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -11,6 +11,7 @@ var Header = React.createClass({
                        <li><img src="images/pluralsight-logo.png"/></li>
                        <li><a href="/#home">Home</a></li>
                        <li><a href="/#about">About</a></li>
+                       <li><a href="/#authors">Authors</a></li>
                    </ul>
                </div>
            </nav>
